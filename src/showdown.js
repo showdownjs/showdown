32db2585116254c1c4ca5364be38caceb9aca738
Joe Martin <joem@numberfour.eu>
2012-06-21 15:30:33 +0200

    Added AMD friendly piece of code.

--- src/showdown.js
+++ src/showdown.js
@@ -1442,3 +1442,12 @@ var escapeCharacters_callback = function(wholeMatch,m1) {
 
 // export
 if (typeof module !== 'undefined') module.exports = Showdown;
+
+// stolen from AMD branch of underscore
+// AMD define happens at the end for compatibility with AMD loaders
+// that don't enforce next-turn semantics on modules.
+if (typeof define === 'function' && define.amd) {
+    define('showdown', function() {
+        return Showdown;
+    });
+}

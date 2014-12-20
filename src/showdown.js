fe7ba8e0d47f35c9914391728d576d546669c0a2
liudng <liudng@gmail.com>
2014-12-20 17:40:19 +0800

    Embedded in italics must add a space in front of the first asterisk and add a space after the second asterisk.

--- src/showdown.js
+++ src/showdown.js
@@ -1133,7 +1133,10 @@ var _DoItalicsAndBold = function(text) {
 	text = text.replace(/(\*\*|__)(?=\S)([^\r]*?\S[*_]*)\1/g,
 		"<strong>$2</strong>");
 
-	text = text.replace(/(\*|_)(?=\S)([^\r]*?\S)\1/g,
+	text = text.replace(/^(\*|_)(?=\S)([^\r]*?\S)\1/g,
+		"<em>$2</em>");
+
+	text = text.replace(/ (\*|_)(?=\S)([^\r]*?\S)\1 /g,
 		"<em>$2</em>");
 
 	return text;

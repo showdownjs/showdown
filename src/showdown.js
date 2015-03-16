36daa9933926dcf6b62aa08bc0114a9bf52bee4f
Hannah Wolfe <erisds@gmail.com>
2015-03-16 16:25:54 +0000

    Add missing block elements
    fixes #90

--- src/showdown.js
+++ src/showdown.js
@@ -375,7 +375,7 @@ Showdown.converter = function (converter_options) {
          )						// attacklab: there are sentinel newlines at end of document
          /gm,function(){...}};
          */
-        text = text.replace(/^(<(p|div|h[1-6]|blockquote|pre|table|dl|ol|ul|script|noscript|form|fieldset|iframe|math|style|section|header|footer|nav|article|aside)\b[^\r]*?<\/\2>[ \t]*(?=\n+)\n)/gm, hashElement);
+        text = text.replace(/^(<(p|div|h[1-6]|blockquote|pre|table|dl|ol|ul|script|noscript|form|fieldset|iframe|math|style|section|header|footer|nav|article|aside|address|audio|canvas|figure|hgroup|output|video)\b[^\r]*?<\/\2>[ \t]*(?=\n+)\n)/gm,hashElement);
 
         // Special case just for <hr />. It was easier to make a special case than
         // to make the other regex more complicated.

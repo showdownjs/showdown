bb52920b9b2b48bfeb2ccf0af3cd11b63a7e55ae
mc007 <asd28071977>
2016-11-18 12:14:14 +0100

    =add electron support

--- src/loader.js
+++ src/loader.js
@@ -1,9 +1,10 @@
 var root = this;
 
+var _window = typeof window !== 'undefined' ? window : null;
+var isElectron = _window && _window.process && _window.process.type === "renderer";
 // CommonJS/nodeJS Loader
-if (typeof module !== 'undefined' && module.exports) {
+if (typeof module !== 'undefined' && module.exports && !isElectron) {
   module.exports = showdown;
-
 // AMD Loader
 } else if (typeof define === 'function' && define.amd) {
   define(function () {

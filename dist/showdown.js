113f5f64b12a011ee2658e0cdba15a556dd557aa
Estevao Soares dos Santos <estevao.santos@gmail.com>
2016-12-20 22:08:33 +0000

    fix(horizontal rule): revert backwards incompatibility change
    Horizontal rule syntax allows up to 3 spaces preceding dashes or
    asterisks. Commit da8fb53 wrongfully removed that. This commit
    puts that back.
    
    Closes #317

--- dist/showdown.js
+++ dist/showdown.js
@@ -1258,9 +1258,9 @@ showdown.subParser('blockGamut', function (text, options, globals) {
 
   // Do Horizontal Rules:
   var key = showdown.subParser('hashBlock')('<hr />', options, globals);
-  text = text.replace(/^( ?-){3,}[ \t]*$/gm, key);
-  text = text.replace(/^( ?\*){3,}[ \t]*$/gm, key);
-  text = text.replace(/^( ?_){3,}[ \t]*$/gm, key);
+  text = text.replace(/^ {0,2}( ?-){3,}[ \t]*$/gm, key);
+  text = text.replace(/^ {0,2}( ?\*){3,}[ \t]*$/gm, key);
+  text = text.replace(/^ {0,2}( ?_){3,}[ \t]*$/gm, key);
 
   text = showdown.subParser('lists')(text, options, globals);
   text = showdown.subParser('codeBlocks')(text, options, globals);

9a9b41f906fc74fcbe286feb4ef489903d2b8b2e
Estevao Soares dos Santos <estevao.santos@gmail.com>
2017-12-14 12:48:41 +0000

    update

---
+++ blog/annoucement-showdown-2.0.md
@@ -0,0 +1,65 @@
+«««
+title: Annoucement: Showdown 2.0,
+author: Estevão Soares dos Santos,
+author_avatar: img/avatars/tivie.jpg,
+date: 2017-12-12,
+language: en,
+image: img/blog/2017.12.12.jpg,
+summary: Showdown version 1.0.0 was released almost 3 years ago, back in May 2015.
+    Since then, it has seen a lot of improvements, with a boost to performance and a significant number of features now
+    included in the core of the library. However, do to backwards compatibility constrains, we are still supporting a
+    lot of legacy features, that prevent us from moving the software forward.
+    With that in mind, **it is time to start thinking about Showdown version 2.0.**
+    For the next major release of Showdown we have planned a range of **significant changes and additions**.
+»»»
+Showdown version 1.0.0 was released almost 3 years ago, back in May 2015.
+Since then, it has seen a lot of improvements, with a boost to performance and
+a significant number of features now included in the core of the library.
+
+However, do to backwards compatibility constrains, we are still supporting a lot
+of legacy features, that prevent us from moving the software forward.
+
+With that in mind, **it is time to start thinking about Showdown version 2.0.**
+
+For the next major release of Showdown we have planned a range of **significant changes
+and additions**. Here are a few of the most important ones:
+
+## Reverse Conversion (HTML to MD)
+
+Being a popular request by our users, we though that a new major version is the perfect
+opportunity to implement this feature.
+
+This feature requires a number of changes throughout the code and API. For instance,
+the extension system must be redesigned to allow for extensions to hook unto both
+processes (HTML->MD and MD->HTML). Event and option names must also reflect this
+since, as they stand now, they might cause confusion.
+
+## Extensions
+
+We are (finally) **completely dropping support for legacy extensions**. Although there are a
+couple old ones still in the wild, most of them (if not all) have already
+migrated to the modern extension system.
+
+We are also revamping the extension system. For starters, **we're dropping old "lang"
+and "output" extension in favour of event extensions**.
+
+The **API of event extensions will also change** in order to mimic the browser events
+and play nicely with other libraries and frameworks.
+
+## Subparsers
+
+**Subparsers will also see a major code refactoring**, with performance improvements.
+Although performance is no longer an issue, mainly due to the way new browsers
+handle Regular Expressions, there are a couple of issues that still need to be addressed.
+Most of the issues can be fixed if we drop support for old browsers and old nodejs versions.
+
+## Development
+
+Formally, Showdown 2.0 will start development in the beginning of 2018 and all
+development efforts will be put into it.
+This means **Showdown 1.x will now enter maintenance mode, that is, no new features
+will be added** and only important bugfixes will be committed.
+
+We expect to release a stable version of Showdown 2.0 somewhere around June 2018,
+but an alpha-version might be released a lot sooner. ***So, if you are as excited as
+we are, stay tuned for more information***.
--- js/app.js
+++ js/app.js
@@ -1,71 +1,173 @@
-latestVersion = 0;
+var converter = new showdown.Converter({
+  metadata: true,
+  tables: true,
+  strikethrough: true,
+  ellipsis: true,
+  headerLevelStart: 3
+});
+
+/** ROUTING **/
+var router = new Navigo('http://showdownjs.com/', true, '#!');
+/** BLOG POSTS **/
+var blogPosts = {};
 
 $(document).ready(function() {
+  blogPosts = new BlogPosts();
 
-  getLatestVersion();
+  function BlogPosts () {
+    var posts = [];
+    var indexedList = {};
 
-  function loadPage(url) {
-    $('#page')
-      .load(url, function () {
-        console.log('navigate to ' + url);
-        $('#page').find('pre>code').each(function(i, block) {
-          hljs.highlightBlock(block);
-          hljs.lineNumbersBlock(block);
+    var getList = this.getList = function() {
+      var dfd = jQuery.Deferred();
+      if (posts.length !== 0) {
+        dfd.resolve(posts);
+      } else {
+        $.getJSON('blog/posts.json', function(data) {
+          for (var i = 0; i < data.length; ++i) {
+            data[i].metadata.summary = converter.makeHtml(data[i].metadata.summary);
+          }
+          dfd.resolve(data);
         });
-      })
+      }
+      return dfd.promise();
+    };
+
+    this.getPostByCanonical = function(canonical) {
+      var dfd = jQuery.Deferred();
+      var idx = null;
+
+      if (typeof indexedList[canonical] !== 'undefined') {
+        idx = indexedList[canonical];
+      }
+
+      $.when(getList()).then(function(list) {
+        if (idx !== null) {
+          dfd.resolve(list[idx]);
+        } else {
+          for (var i = 0; i < list.length; ++i) {
+            if (list[i].canonical === canonical) {
+              var promise = $.ajax({
+                url: list[i].url,
+                dataType: "text"
+              });
+              promise.done((function (index, listItem) {
+                return function (md) {
+                  listItem.post = converter.makeHtml(md);
+                  posts[index] = listItem;
+                  indexedList[canonical] = index;
+                  dfd.resolve(listItem);
+                };
+              })(i, list[i]));
+              return;
+            }
+          }
+          dfd.resolve(null);
+        }
+      });
+      return dfd.promise();
+    };
   }
 
-  function getCurrentUrl (router) {
-    var uriSplit = window.location.href.split('#!');
-    var defaultShebang = '#!/home';
-    var shebang = '';
-    var currentUrl = '';
-    if (typeof uriSplit[1] === 'undefined') {
-      shebang = defaultShebang;
-    } else {
-      shebang = '#!' + uriSplit[1];
-    }
-    if (router.hasOwnProperty(shebang)) {
-      currentUrl = router[shebang];
-    } else {
-      currentUrl = router[defaultShebang];
-    }
-    return currentUrl;
-  }
+  $(document).ajaxStart(function(){
+    $.LoadingOverlay('show', {
+      image: '',
+      //color: 'rgba(166, 200, 157, 0.7)',
+      color: '#fff',
+      fontawesome: 'fa fa-spinner fa-spin'
+    });
+  });
+  $(document).ajaxStop(function(){
+    $.LoadingOverlay("hide");
+  });
 
   function getLatestVersion() {
-    $.getJSON('https://api.github.com/repos/showdownjs/showdown/releases/latest', function (data) {
-      $('#lt-version-num').html('v' + data.tag_name);
-      $('#lt-version-lnk').attr('href', data.zipball_url);
-      latestVersion = data.tag_name;
+    var dfd = jQuery.Deferred();
+
+    if (typeof Cookies.get('version') === 'undefined' || typeof Cookies.get('zipball') === 'undefined') {
+      $.getJSON('https://api.github.com/repos/showdownjs/showdown/releases/latest', function (data) {
+        var version = data.tag_name;
+        var zipball = data.zipball_url;
+        Cookies.set('version', version, { expires: 0.1 });
+        Cookies.set('zipball', zipball, { expires: 0.1 });
+        dfd.resolve({version: version, zipball: zipball});
+      });
+    } else {
+      dfd.resolve({
+        version: Cookies.get('version'),
+        zipball: Cookies.get('zipball')
+      });
+    }
+    return dfd.promise();
+  }
+
+  function loadPage(url) {
+    var $page = $('#page');
+    return $page.load(url, function () {
+      $page.find('pre>code').each(function(i, block) {
+        hljs.highlightBlock(block);
+        hljs.lineNumbersBlock(block);
+        router.updatePageLinks();
+      });
     });
   }
 
+  function injectParams(params) {
+    if (typeof params !== 'undefined') {
+      // pass params to page
+      for (var param in params) {
+        if (params.hasOwnProperty(param)) {
+          var $pageParams = $('#page-params');
+          $pageParams.html();
+          $pageParams.append('<data id="param-' + param + '" value="'+ params[param] +'" hidden style="display: none;"></data>');
+          console.log(params[param]);
+        }
+      }
+    }
+  }
 
-
-  $('.internal-navigation').click(function(evt) {
+  function changeActiveLink(name) {
     $('.internal-navigation').removeClass('active');
-    $(this).addClass('active');
+    $('#navlink-' + name).addClass('active');
+  }
 
-    var url = $(this).attr('data-href');
-    //load page
-    loadPage(url);
+  $.when(getLatestVersion()).then(
+    function(data) {
+      $('#lt-version-num').html('v' + data.version);
+      $('#lt-version-lnk').attr('href', data.zipball);
+    }
+  );
 
+  router
+    .on('/releases', function () {
+      changeActiveLink('releases');
+      return loadPage('html/releases.html');
+    })
+    .on('/documentation', function () {
+      changeActiveLink('documentation');
+      return loadPage('html/documentation.html');
+    })
+    .on('/blog/:canonical', function (params) {
+      changeActiveLink('blog');
+      injectParams(params);
+      return loadPage('html/blog-article.html', params);
+    })
+    .on('/blog', function () {
+      changeActiveLink('blog');
+      return loadPage('html/blog.html');
+    })
+    .on('*', function () {
+      changeActiveLink('home');
+      return loadPage('html/main.html');
+    })
+    .resolve();
+
+
+  $('.internal-navigation').click(function() {
     // smooth scroll to page
     $('html, body').animate({
       scrollTop: $('#page').offset().top
     }, 500);
-  });
+  })
 
-
-  var router = {};
-
-
-  $('.internal-navigation').each(function () {
-    var shebang = $(this).attr('href');
-    router[shebang] = $(this).attr('data-href');
-  });
-
-  // load page on startup
-  loadPage(getCurrentUrl(router));
 });

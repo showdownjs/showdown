81edc70da7372f662954321d4b372e1eae189870
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2018-10-21 15:20:39 +0200

    fix(underline): Make underline lazy
    fix underline if literalMidwordUnderscores option is enabled
    it should end at the nearest closing underscores, not the furthest

--- dist/showdown.js
+++ dist/showdown.js
@@ -1,4 +1,4 @@
-;/*! showdown v 2.0.0-alpha1 - 25-09-2018 */
+;/*! showdown v 2.0.0-alpha1 - 21-10-2018 */
 (function(){
 /**
  * Created by Tivie on 13-07-2015.
@@ -4309,11 +4309,17 @@ showdown.subParser('makehtml.underline', function (text, options, globals) {
   text = globals.converter._dispatch('makehtml.underline.before', text, options, globals).getText();
 
   if (options.literalMidWordUnderscores) {
-    text = text.replace(/\b_?__(\S[\s\S]*)___?\b/g, function (wm, txt) {
+    text = text.replace(/\b___(\S[\s\S]*?)___\b/g, function (wm, txt) {
+      return '<u>' + txt + '</u>';
+    });
+    text = text.replace(/\b__(\S[\s\S]*?)__\b/g, function (wm, txt) {
       return '<u>' + txt + '</u>';
     });
   } else {
-    text = text.replace(/_?__(\S[\s\S]*?)___?/g, function (wm, m) {
+    text = text.replace(/___(\S[\s\S]*?)___/g, function (wm, m) {
+      return (/\S$/.test(m)) ? '<u>' + m + '</u>' : wm;
+    });
+    text = text.replace(/__(\S[\s\S]*?)__/g, function (wm, m) {
       return (/\S$/.test(m)) ? '<u>' + m + '</u>' : wm;
     });
   }
--- test/functional/makehtml/cases/features/underline/simple.md
+++ test/functional/makehtml/cases/features/underline/simple.md
@@ -5,3 +5,5 @@ __an underlined sentence__
 ___three underscores are fine___
 
 _single_ underscores are left alone
+
+__multiple__ underlines in a __paragraph__

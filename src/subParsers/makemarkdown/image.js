1da8481fbea1bdda334b7209e0cfe1c991ce3db0
ggboy-shakalaka <switch2018@outlook.com>
2018-11-17 20:28:32 +0800

    fix bug width|height = auto
    when <img src="a" width="auto" height="auto" /> then ![null](<a> =autoxauto)
    after fix ![null](<a> =*x*)

--- src/subParsers/makemarkdown/image.js
+++ src/subParsers/makemarkdown/image.js
@@ -2,11 +2,13 @@ showdown.subParser('makeMarkdown.image', function (node) {
   'use strict';
 
   var txt = '';
-  if (node.hasAttribute('src')) {
+  if (node.hasAttribute('src') && node.getAttribute('src') !== '') {
     txt += '![' + node.getAttribute('alt') + '](';
     txt += '<' + node.getAttribute('src') + '>';
     if (node.hasAttribute('width') && node.hasAttribute('height')) {
-      txt += ' =' + node.getAttribute('width') + 'x' + node.getAttribute('height');
+      var width = node.getAttribute('width');
+      var height = node.getAttribute('height');
+      txt += ' =' + (width === 'auto' ? '*' : width) + 'x' + (height === 'auto' ? '*' : height);
     }
 
     if (node.hasAttribute('title')) {

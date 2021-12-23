ed51972315c47b7e3341af54b51f8fe07ad90a53
Daniel <complynx@yandex.ru>
2021-12-23 22:10:23 +0300

    fix(helpers): allow usage in ES6 modules fix for #676
    * Fix for issue #676
    
    This will check for situations when this is undefined, eg ES6 module wrapper
    
    Co-authored-by: SyntaxRules <devyn.stott@gmail.com>

--- src/helpers.js
+++ src/helpers.js
@@ -6,11 +6,15 @@ if (!showdown.hasOwnProperty('helper')) {
   showdown.helper = {};
 }
 
-if (typeof this.document === 'undefined' && typeof this.window === 'undefined') {
-  var jsdom = require('jsdom');
-  this.window = new jsdom.JSDOM('', {}).window; // jshint ignore:line
+if (typeof this === 'undefined' && typeof window !== 'undefined') {
+  showdown.helper.document = window.document;
+} else {
+  if (typeof this.document === 'undefined' && typeof this.window === 'undefined') {
+    var jsdom = require('jsdom');
+    this.window = new jsdom.JSDOM('', {}).window; // jshint ignore:line
+  }
+  showdown.helper.document = this.window.document;
 }
-showdown.helper.document = this.window.document;
 
 /**
  * Check if var is string
